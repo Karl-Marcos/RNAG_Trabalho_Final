c950f000a1899c199c207bfe134649a559fae23b
Marcos de Cerqueira Leite Pimentel <marcos220057@ilum.cnpem.br>
2023-06-06 17:19:32 -0300

    codigo funcionando com problemas

--- funcoes.py
+++ funcoes.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-def malthus(t,x,a):
+def malthus(t,x,k):
     """Equação diferencial de Malthus
     """
-    return a*x
+    return k*x
 
 def verhulst(t,x,a):
     """Equação diferencial de Verhulst
@@ -15,14 +15,22 @@ def gompertz(t,x, a):
     """
     return a * x * np.log(1/x)
 
-def runge_kutta(eq, t0, x0, tf, h, a):
-    t = np.arange(t0, tf + h, h)
-    x = np.zeros(len(t))
-    x[0] = x0
-    for i in range(1, len(t)):
-        k1 = eq(t[i-1], x[i-1], a)
-        k2 = eq(t[i-1] + h/2, x[i-1] + k1/2, a)
-        k3 = eq(t[i-1] + h/2, x[i-1] + k2/2, a)
-        k4 = eq(t[i-1] + h, x[i-1] + k3, a)
-        x[i] = x[i-1] + h * (k1 + 2*k2 + 2*k3 + k4)/6
-    return t, x
+
+def runge_kutta(dx, t0, x0, tf, h, k):
+    t_values = [t0]  # List to store time values
+    x_values = [x0]  # List to store x values
+
+    while t0 < tf:
+        if t0 + h > tf:
+            h = tf - t0  # Adjust the step size for the last iteration if necessary
+        k1 = dx(t0, x0, k)
+        k2 = dx(t0 + h/2, x0 + h/2 * k1, k)
+        k3 = dx(t0 + h/2, x0 + h/2 * k2, k)
+        k4 = dx(t0 + h, x0 + h * k3, k)
+
+        x0 = x0 + (h/6) * (k1 + 2*k2 + 2*k3 + k4)  # Update x using the Runge-Kutta formula
+        t0 += h  # Increment the time
+        t_values.append(t0)  # Add current time and x value to the lists
+        x_values.append(x0)
+
+    return np.array(t_values), np.array(x_values)
